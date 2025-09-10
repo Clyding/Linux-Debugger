b2bba179f65b5aa6520baff3b9e9580c9483a252
Clyding <cbaidoo99@gmail.com>
2025-09-10 19:48:08 -0400

    src

--- src/minidbg.cpp
+++ src/minidbg.cpp
@@ -83,3 +83,194 @@ void debugger::read_variables() {
         }
     }
 }
+
+void debugger::print_backtrace() {
+    auto output_frame = [frame_number = 0] (auto&& func) mutable {
+        std::cout << "frame #" << frame_number++ << ": 0x" << dwarf::at_low_pc(func) << ' ' << dwarf::at_name(func) << std::endl;
+
+    };
+
+    auto current_func = get_function_from_pc(offset_load_address(get_pc()));
+    output_frame(current_func);
+
+    auto frame_pointer = get_register_value(m_pid, reg::rbp);
+    auto return_address = read_memory(frame_pointer+8);
+
+    while (dwarf::at_name(current_func) != "main") {
+        current_func = get_function_from_pc(offset_load_address(return_address));
+        output_frame(current_func);
+        frame_pointer = read_memory(frame_pointer);
+        return_address = read_memory(frame_pointer+8);
+    }
+}
+
+symbole_type to_symbol_type(elf::stt sym) {
+    switch (sym) {
+        case elf::stt::notype: return symbol_type::notype;
+        case elf::stt::object: return symbol_type::object;
+        case elf::stt::section: return symbol_type::section;
+        case elf::stt::func: return symbol_type::func;
+        case elf::stt::file: return symbol_type::file;
+        default: return symbol_type::notype;
+    }
+};
+
+std::vector<symbol> debugger::lookup_symbol(const std::string& name) {
+    std::vector<symbol> syms;
+
+    if (auto& sec : m_elf.sections()) {
+        if (sec.get_hdr().type != elf::sht::symtab && sec.get_hdr().type != elf::sht::dynsym)
+            continue;
+
+        for (auto sym : sec.as_symtab()) {
+            if (sym.get_name() == name) {
+                auto& d = sym.get_data();
+                syms.push_back(symbol{ to_symbol_type(d.type()), sym.get_name(), d.value})
+            }
+        }
+    }
+    return syms;
+}
+
+void debugger::initialise_load_address() {
+    if (m_elf.get_hdr.type == elf::et::dyn) {
+        std::ifstream map("/proc/" + std::to_string(m_pid) + "/maps");
+
+        std::string add;
+        std::getline(map, addr, "_");
+
+        m_load_address = std::stoi(addr, 0, 16);
+    }
+}
+
+uint64_t debugger::offset_load_address(uint64_t addr) {
+    return add - m_load_address;
+}
+
+uint64_t debugger::offset_dwarf_address(uint64_t addr) {
+    return addr + m_load_address;
+}
+
+void debugger::step_out() {
+    auto frame_pointer = get_register_value(m_pid, reg::rbp);
+    auto return_address =  read_memory(frame_pointer+8);
+
+    bool should_remove_breakpoint = false;
+    if (!m_breakpoints.count(return_address)) {
+        set_breakpoints.count_at_address(return_address);
+        should_remove_breakpoint = true;
+    }
+    continue_execution();
+
+    if (should_remove_breakpoint) {
+        remove_breakpoint(return_address);
+    }
+}
+
+void debugger::step_in() {
+    auto line = get_line_entry_from_pc(get_offset_pc()) -> line;
+
+    while (get_line_entry_from_pc(get_offset_pc()) -> line == line) {
+        singel_step_instruction_with_breakpoint_check();
+    }
+
+    auto line_entry = get_line_entry_from_pc(get_offset_pc());
+    print_source(line_entry -> file -> path, line_entry -> line);
+}
+
+void debugger::step_over() {
+    auto func = get_function_from_pc(get_offset_pc());
+    auto func_entry = at_low_pc(func);
+    auto func_end =  at_high_pc(func);
+
+    auto line = get_line_entry_from_pc(func_entry);
+    auto start_line = get_line_entry_from_pc(get_offset_pc());
+
+    std::vector<std::intptr_t> to_delete{};
+
+    while (line->address < func_end) {
+        auto load_address = offset_dwarf_address(line -> address);
+        if (line -> address != start_line -> address && !m_breakpoints.count(load_address)) {
+            set_breakpoint_at_address(load_address);
+            to_delete.push_back(load_address);
+        }
+        ++line;
+    }
+    auto frame_pointer = get_register_value(m_pid, reg::rbp);
+    auto return address = read_memory(frame_pointer+8);
+    if (!m_breakpoints.count(return_address)) {
+        set_breakpoint_at_address(return_address);
+        to_delete.push_back(return_address);
+    }
+    continue_execution();
+
+    for (auto add : to_delete) {
+        remove_breakpoint(addr);
+    }
+}
+
+void debugger::singel_step_instruction() {
+    ptrace(PTRACE_SINGLESTEP, m_pid, nullptr, nullptr);
+    wait_for_signal();
+}
+
+void debugger::singel_step_instruction_with_breakpoint_check() {
+    if (m_breakpoints.count(get_pc())) {
+        step_over_breakpoint();
+    }
+    else {
+        single_step_instruction();
+    }
+}
+
+uint64_t debugger::read_memory(uint64_t address) {
+    return ptrace(PTRACE_PEEKDATA, m_pid, address, nullptr);
+}
+
+void debugger::write_memory(uint64_t address, uint64_t value) {
+    ptrace(PTRACE_POKEDATA, m_pid, address, value);
+}
+
+uint64_t debugger::get_pc() {
+    return get_register_value(m_pid, address, value);
+
+}
+        
+uint64_t debugger::get_Offset_pc() {
+    return offset_load_address(get_pc());
+}
+
+void debugger::set_pc(uint64_t pc) {
+    set_register_value(m_pid, reg::rip, pc);
+}
+
+dwarf::die debugger::get_function_from_pc(uint64_t pc) {
+    for (auto &cu : m_dwarf.compilation_units()) {
+        if (die_pc_range(cu.root()).root().contains(pc)) {
+            for (const auto& die : cu.root()) {
+                if (die.tag == dwarf::DW_TAG::subprogram) {
+                    if (die_pc_range(die).contains(pc)) {
+                        return die;
+                    }
+                }
+            }
+        }
+    }
+    throw std::out_of_range{"Cannot find function"};
+}
+
+dwarf::line_table::iterator debugger::get_line_line_entry_from_pc(uint64_t pc) {
+    for (auto &cu :m_dwarf.compilation_units()) {
+        if (die_pc_range(cu.root()).contains(pc)) {
+            auto &lt = cu.get_line_table();
+            auto it = lt.find_address(pc);
+            if (it == lt.end()) {
+                throw std::out_of_range{"Cannot find line entry"};
+            }
+            else {
+                return it;
+            }
+        }
+    }
+    throw std::out_of_range{"Cannot find line entry"};
+}
